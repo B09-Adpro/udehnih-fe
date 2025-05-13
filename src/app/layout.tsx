c64ed8b680f74313c475a71b50fc9f5d21c58403
gracekarinn <gracekarina12@gmail.com>
2025-05-13 19:06:03 +0700

    feat: wrap children in main tag and include Toaster component in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
+import { Toaster } from '@/components/ui/sonner';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +31,10 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50`}
       >
         <Navbar />
-        {children}
+         <main>
+            {children}
+         </main>
+         <Toaster />
         <Footer />
       </body>
     </html>
