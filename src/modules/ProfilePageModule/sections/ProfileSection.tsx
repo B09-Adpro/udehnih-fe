1cdfd78434471ddf092dfe7cb56b7de8aa12f68b
sdikyarts <wtrcrss.family@gmail.com>
2025-05-22 19:11:31 +0700

    feat: add user role to profile section

--- src/modules/ProfilePageModule/sections/ProfileSection.tsx
+++ src/modules/ProfilePageModule/sections/ProfileSection.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
 
 export const ProfileSection = () => {
     return (
@@ -11,18 +12,19 @@ export const ProfileSection = () => {
                     <div className="relative w-full aspect-square max-w-[180px]">
                         <Avatar className="w-full h-full relative flex shrink-0 overflow-hidden rounded-full">
                             <AvatarImage src="/placeholder-avatar.jpg" alt="Profile Picture" className="aspect-square h-full w-full object-cover" />
-                            <AvatarFallback className="flex h-full w-full items-center justify-center rounded-full bg-gray-200">
-                                <span className="text-6xl">SK</span>
-                            </AvatarFallback>
+                            <AvatarFallback className="flex h-full w-full items-center justify-center rounded-full bg-gray-200"/>
                         </Avatar>
                     </div>
-                    <div className="space-y-4">
+                    <div className="space-y-2">
                         <h1 className="text-4xl md:text-5xl font-bold tracking-tight">
                             Stray Kids
                         </h1>
-                        <p className="text-gray-600 mb-6 text-lg">
+                        <p className="text-gray-600 mb-4 text-lg">
                             straykids@jype.com
                         </p>
+                        <Badge className="font-medium text-md bg-primary/10 text-primary border-0">
+                            Student
+                        </Badge>
                     </div>
                 </div>
             </div>
