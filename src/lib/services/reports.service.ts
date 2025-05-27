dc7e99b15caab57a45710b9dccce067c92e0efdd
gracekarinn <gracekarina12@gmail.com>
2025-05-27 11:36:44 +0700

    fix: typo

--- src/lib/services/reports.service.ts
+++ src/lib/services/reports.service.ts
@@ -3,7 +3,7 @@ import { ReportRequestDto, ReportResponseDto, AuthUser } from './interface';
 import { AuthService } from './auth.service';
 
 
-const API_URL = process.env.NEXT_PUBLIC_REPORT_API_URL|| 'http://localhost:8000';
+const API_URL = process.env.NEXT_PUBLIC_REPORT_API_URL || 'http://localhost:8000';
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
